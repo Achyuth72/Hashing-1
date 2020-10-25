4d7f185df7f86441fe2e9af3a710ae5450299264
Achyuth72 <68582633+Achyuth72@users.noreply.github.com>
2020-10-25 15:15:54 -0600

    Create  Isomorphic Strings.java

---
+++ Isomorphic Strings.java
@@ -0,0 +1,47 @@
+//time complexity:O(n)
+//space complexity:O(n)
+//did u have any problem running the problem on leetcode:No
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        HashMap<Character,Character> smap=new HashMap<>();
+        HashMap<Character,Character> tmap=new HashMap<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char schar=s.charAt(i);
+            char tchar=t.charAt(i);
+            
+            if(smap.containsKey(schar)) 
+            {
+                if(smap.get(schar)!=tchar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                smap.put(schar,tchar);
+            }
+            
+           if(tmap.containsKey(tchar)) 
+            {
+                if(tmap.get(tchar)!=schar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                tmap.put(tchar,schar);
+            }
+            
+        }
+        return true;
+        
+    }
+}
